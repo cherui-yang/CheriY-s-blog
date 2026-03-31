88bc2067a4082c0b432923f46b1e68e641125db7
cherui-yang <1197498526@qq.com>
2026-03-31 20:01:50 +0800

    Add VitePress configuration for personal website

---
+++ .vitepress/config.mjs
@@ -0,0 +1,14 @@
+export default {
+  title: "我的网站",
+  description: "个人学习笔记",
+  themeConfig: {
+    sidebar: [
+      {
+        text: "学习笔记",
+        items: [
+          { text: "首页", link: "/" },
+        ],
+      },
+    ],
+  },
+};
