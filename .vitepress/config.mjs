453fc7f80c7e0d2498b1fbe08689385891c05f68
cherui-yang <1197498526@qq.com>
2026-03-31 20:47:38 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,24 @@ export default {
   base: "/CheriY-s-blog/",
 
   themeConfig: {
-    sidebar: false // 👈 只需要写 false，就会自动生成！
+    sidebar: [
+      {
+        text: "首页",
+        link: "/index.md"
+      },
+      {
+        text: "专业课",
+        items: [
+          { text: "自动控制原理", link: "/专业课/自动控制原理.md" },
+        ]
+      },
+      {
+        text: "编程学习",
+        items: [
+          { text: "Python入门", link: "/编程学习/Python.md" },
+        ]
+      }
+    ]
   },
 
   markdown: {
