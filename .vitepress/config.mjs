9917d4b62a722ae5eb06d8c941ea5bc4dd4311e2
cherui-yang <1197498526@qq.com>
2026-03-31 20:45:31 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,20 +1,12 @@
-import { getSidebar } from 'vitepress-plugin-auto-sidebar'
-
 export default {
   title: "CheriY's Blog",
-  description: "我的个人学习笔记",
+  description: "个人学习笔记",
   base: "/CheriY-s-blog/",
 
   themeConfig: {
-    // 🔥 重点：这行代码自动生成侧边栏
-    sidebar: getSidebar({
-      contentDir: '/docs',
-      collapse: false,
-      collapseDepth: 2
-    }),
+    sidebar: false // 👈 只需要写 false，就会自动生成！
   },
 
-  // 支持数学公式
   markdown: {
     math: true
   }
